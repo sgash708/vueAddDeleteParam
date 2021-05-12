895cf34da8bd437e7c8982c39c2538797a243f3f
sugar sato <57472355+sgash708@users.noreply.github.com>
2021-05-12 10:20:42 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <Paramedit />
+  </div>
+</template>
+
+<script>
+import Paramedit from "./components/Paramedit.vue";
+
+export default {
+  name: "App",
+  components: {
+    Paramedit,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Paramedit.vue
@@ -0,0 +1,63 @@
+<template>
+  <form id="form">
+    <table border="1">
+      <tr v-for="(sec, index) in task.params" :key="index">
+        <td>Name</td>
+        <td><input v-model="sec.Name" /></td>
+        <td>Path</td>
+        <td><input v-model="sec.ValueFrom" /></td>
+        <td><button @click.prevent="deleteParam(index)">削除</button></td>
+      </tr>
+    </table>
+    <a @click.prevent="addParam">環境変数の追加</a>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 更新対象
+      task: {
+        params: [
+          { Name: "test1", ValueFrom: "test/path/1" },
+          { Name: "test2", ValueFrom: "test/path/2" },
+        ],
+      },
+    };
+  },
+  created() {},
+  methods: {
+    deleteParam: function (id) {
+      let self = this;
+      self.task.params.splice(id, 1);
+    },
+    addParam: function () {
+      let self = this;
+      let addForm = {
+        Name: "",
+        ValueFrom: "",
+      };
+      self.task.params.push(addForm);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
